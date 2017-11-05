d538b6a31a4eb0e6e230f81f576615b8964c81cf
RyanExia <ryanmata10@gmail.com>
2017-11-05 16:21:50 +0100

    soustraction

--- main.c
+++ main.c
@@ -8,5 +8,6 @@ int addition (int a ,int b)
 int main ()
 {
    printf("L’addition de a et de b vaut %d\n", addition(1,4));
+   printf("L'addition de a et de b vaut %d\n",soustraction(1,4))
     return 0;
     }
